a10d0cdeaf7429d47bb4f96fca9e33d142c503de
Sergio <sergio.ayala9208@gmail.com>
2024-02-19 21:26:43 -0500

    se agregan cambios en el controlador y se agrega información en el readme

--- src/main/java/com/example/javaspringmarket/domain/controller/ProductController.java
+++ src/main/java/com/example/javaspringmarket/domain/controller/ProductController.java
@@ -3,10 +3,6 @@ package com.example.javaspringmarket.domain.controller;
 import com.example.javaspringmarket.domain.Product;
 import com.example.javaspringmarket.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,20 +17,13 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
-    @ApiOperation("Get all supermarket products")
-    @ApiResponse(code = 200, message = "OK")
+
     public ResponseEntity<List<Product>> getAll() {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    @ApiOperation("Search a product with an ID")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 404, message = "Product not found"),
-    })
-    public ResponseEntity<Product> getProduct(@ApiParam(value = "The id of the product", required = true, example = "7")
-                                              @PathVariable("id") int productId) {
+    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
